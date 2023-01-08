fd63c1bf10594ce5bc9880a6f5b018af2cd5992b
olga petrova <petrovaolga629@gmail.com>
2023-01-08 23:50:40 +0200

    Добавьте файлы проекта.

---
+++ Isikukood/Isikukood.py
@@ -0,0 +1,12 @@
+﻿from Oma_moodul import parse_isikukood
+
+while True:
+    ik = input("Isikukood: ")
+    try:
+        form_ik = parse_isikukood(ik)
+        print(form_ik)
+    except Exception as e:
+        if e.args[0] != '':
+            print("Andmetüüp on vale, On vaja numbreid sisestada")
+
+
